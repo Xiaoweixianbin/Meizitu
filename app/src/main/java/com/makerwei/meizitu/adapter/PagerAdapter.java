7e16641deb817fa87d0a8ec5db51a3cb16be5754
weixianbin <weixianbin1996@gmail.com>
2016-03-21 21:10:40 +0800

    重构代码

---
+++ app/src/main/java/com/makerwei/meizitu/adapter/PagerAdapter.java
@@ -0,0 +1,24 @@
+package com.makerwei.meizitu.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by weixianbin on 16/3/21.
+ */
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
